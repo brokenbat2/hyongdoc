dfc45c3dbfa932304e6e4a1c1678282075586928
unknown <>
2017-02-26 15:02:40 +0900

    Add 2nd Week Homework

--- 2_week_Django/src/blog/admin.py
+++ 2_week_Django/src/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Article
+from .models import Comment
 
-# Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Article, BlogAdmin)
+admin.site.register(Comment, BlogAdmin)
--- 2_week_Django/src/blog/views.py
+++ 2_week_Django/src/blog/views.py
@@ -1,6 +1,14 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
+
+from .models import Article
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+
+    article_list = Article.objects.all()
+
+    ctx = {
+        "article_list" : article_list
+    }
+    return render(request, "index.html", ctx)
